c9133b0285ed45dba9e59d832f129beb6a264bb4
NateDiggityDogg <32425233+NateDiggityDogg@users.noreply.github.com>
2018-11-15 15:46:53 -0500

    Updating Search Function; actually works now

--- src/reader/ReaderSearch.java
+++ src/reader/ReaderSearch.java
@@ -10,25 +10,31 @@ import java.util.Scanner;
 public class ReaderSearch {
 	static Boolean[] selectedFiles;
 	public static void main() {
-		String searchText = Reader.searchText;
+		String searchText = Reader.searchText.toLowerCase();
 		File fileFolder = new File("").getAbsoluteFile();
 		File[] files = fileFolder.listFiles();
 		selectedFiles = new Boolean[files.length];
 		System.out.println(files.length);
+		System.out.println(searchText);
 		for (int i = 0; i < files.length; i++) {
 			try {
 				Scanner scan = new Scanner(files[i]);
 				Boolean bool = true;
 				
-				if(scan.hasNextLine() && scan.nextLine() == searchText) {
-					selectedFiles[i] = true;
-					System.out.println("Found!!!");
+				if(scan.hasNextLine()) {
+					String comparison = scan.nextLine();
+					if(comparison.contains(searchText)) {
+						System.out.println("Found!!!");
+						selectedFiles[i] = true;
+					}else {
+						System.out.println("Not found!!!" + comparison);
+						selectedFiles[i] = false;
+					}
 				}
-				System.out.println(searchText);
+//				System.out.println(searchText);
 				
 				while (bool == true) {
 					if (scan.hasNextLine()) {
-						System.out.println(i);
 						System.out.println(scan.nextLine());
 					} else {
 						System.out.println("");
@@ -36,10 +42,17 @@ public class ReaderSearch {
 					}
 				}
 				scan.close();
+				
+				System.out.println(selectedFiles.length);
+				
 			} catch (FileNotFoundException e) {
 				e.printStackTrace();
 			}
 		}
+		for(int i=0;i<selectedFiles.length;i++) {
+			System.out.println(selectedFiles[i]);
+		}
+		
 	}
 
 }
